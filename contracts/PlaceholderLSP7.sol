5b003f98ceba6fba00c372c94886af8021fffbad
B00ste <afteni.danu@gmail.com>
2022-07-02 14:29:19 +0300

    Testing frontend initialized.

--- contracts/PlaceholderLSP7.sol
+++ contracts/PlaceholderLSP7.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.0;
 
 import "@lukso/lsp-smart-contracts/contracts/LSP7DigitalAsset/LSP7DigitalAsset.sol";
 
-contract PlaceholderLSP7 is LSP7DigitalAsset("PlaceholderLSP7", "LSP7", msg.sender, true) {
+contract PlaceholderLSP7 is LSP7DigitalAsset {
+
+  constructor(string memory _name, string memory _symbol) LSP7DigitalAsset(_name, _symbol, msg.sender, true) {
+
+  }
 
 }

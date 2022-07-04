715aa3d276b48068fb9a17482fba26696912d1b1
b00ste.lyx <62855857+b00ste@users.noreply.github.com>
2022-07-05 00:57:31 +0300

    Update users.

--- src/Components/Users.tsx
+++ src/Components/Users.tsx
@@ -13,11 +13,17 @@ export const users = [
     name: 'ETHalorian',
     address: '0x5Eb124d50aC103d04758AA819b109E34EE1C0909',
     id: 3
+  },
+  {
+    name: 'JordyDutch',
+    address: '0x69e21698D00737D4b5f4a5a6078964548fcd6701',
+    id: 4
   }
 ]
 
 export const addressesToNames = {
   '0x5678fc0Be54443dAE626b1522db3fb38FeBd8eb5': 'B00ste',
   '0x80A5E4986b94cCE204EC1F24c1b6fA97432339Eb': 'sage',
-  '0x5Eb124d50aC103d04758AA819b109E34EE1C0909': 'ETHalorian'
+  '0x5Eb124d50aC103d04758AA819b109E34EE1C0909': 'ETHalorian',
+  '0x69e21698D00737D4b5f4a5a6078964548fcd6701': 'JordyDutch'
 };
